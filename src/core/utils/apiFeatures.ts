e87c5c65f59386fb9f051549800b1ef3782d5cc9
amin-ep <amincode24@gmail.com>
2025-09-03 10:39:28 +0330

    fix: ApiFeatures

--- src/core/utils/apiFeatures.ts
+++ src/core/utils/apiFeatures.ts
@@ -45,7 +45,7 @@ export default class ApiFeatures {
   }
 
   limitFields() {
-    if (this.queryString.limit) {
+    if (this.queryString.fields) {
       const fields = this.queryString.fields.split(',').join(' ');
       this.query = this.query.select(fields);
     } else {
